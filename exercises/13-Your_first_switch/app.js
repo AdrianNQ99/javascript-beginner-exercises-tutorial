25b4f9b4e78b34837dc0903a99d705cae1df7feb
AdrianNQ99 <aneuville99@gmail.com>
2024-09-22 19:05:45 +0000

    First commit

--- exercises/13-Your_first_switch/app.js
+++ exercises/13-Your_first_switch/app.js
@@ -2,7 +2,10 @@ function getColor(selection)
 {
 	switch(selection){
 		// Add more options here
-	    default:
+	    case "red": return true
+		case "green": return true
+		case "blue": return true
+		default:
 	    	return false;  //returns false because the user picked an unavailable color
 	    break;               
 	}
--- exercises/17-Create_a_for_loop/app.js
+++ exercises/17-Create_a_for_loop/app.js
@@ -1,4 +1,6 @@
 // Declare and write your function here:
-
-
-standardsMaker();
+function standardsMaker(sentence){
+    for(let i=0;i<300;i++)
+        console.log(sentence)
+}
+standardsMaker("I will write questions if I'm stuck");
